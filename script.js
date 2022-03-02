a93e69e9af908468b1a6bc6dd1b7fb0abf93944f
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-02 15:13:32 +0600

    added toggle

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.querySelectorAll('.accordion').forEach(accordion => {
   const parent = accordion('.accordions');
 
   accordion.querySelector('.accordion__header').addEventListener('click', function(){
-    if (parent && parent.classList.contains('accordion'))
+    if (parent && parent.classList.contains('accordions--toggle')){
+      
+    }
     accordion.classList.toggle('accordion--open');
   });
 }); 
