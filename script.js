33cdce277113c99080b492aeaf838843ea7b13ca
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-02 15:43:16 +0600

    added querySelectorAll

--- script.js
+++ script.js
@@ -16,4 +16,4 @@ document.querySelectorAll('[data-modal]').forEach(trigger => {
     document.getElementById(trigger.dataset.modal).classList.add('modal--open');
   });
 });
-document
+document.querySelectorAll
