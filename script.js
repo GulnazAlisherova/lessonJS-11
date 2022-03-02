1a69ee890e93e54ff89d16ffe597358ee488d207
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-02 15:10:10 +0600

    added const

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelectorAll('.accordion').forEach(accordion => {
+  const
+
   accordion.querySelector('.accordion__header').addEventListener('click', function(){
     accordion.classList.toggle('accordion--open');
   });
